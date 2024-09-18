908fb71a2ce95b59214369bc610de3dba97673e5
raselworshop <raselworshop@gmail.com>
2024-09-18 17:49:15 -0400

    added homepage section

--- js/main.js
+++ js/main.js
@@ -3,11 +3,37 @@
 // search for: form submit reloading the page
 
 // step-1: add event handler
+// document.getElementById('btn-login').addEventListener('click', function(event){
+//     // step-2: preven default behavior reloading browser
+//     event.preventDefault();
+//     //step-3: get the phone number & pin number
+//     const phoneNumber = document.getElementById('p-number');
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber.value);
+//     console.log(pinNumber);
+    
+//     //step-4: validate the user input
+//     // temporary used condtions not correct way
+//     if(phoneNumber == '0-10' && pinNumber == 'abc123'){
+//         // step-5: user allowed to enter website 
+//         console.log('You are logged in')
+//     }else{
+//         alert('You typed wrong number or password')
+//     }
+// })
+
 document.getElementById('btn-login').addEventListener('click', function(event){
-    // step-2: preven default behavior reloading browser
-    event.preventDefault();
-    //step-3: get the phone number 
-    const phoneNumber = document.getElementById('p-number');
-    console.log(phoneNumber.value)
-    console.log('login button clicked')
+ event.preventDefault();
+//  get phone number 
+const phoneNumber = document.getElementById('p-number').value;
+console.log(phoneNumber)
+const pinNumber = document.getElementById('pin-number').value;
+console.log(pinNumber);
+
+if( phoneNumber === '5' && pinNumber === '1234'){
+    console.log('You are logged in');
+    window.location.href = './home.html'
+}else{
+    alert('wrong pin or number')
+}
 })
